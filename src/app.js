553870f6dd51a546e3cd10a71658aa02070fa56b
sagarshukla <sagarshukla010@github.com>
2022-08-20 01:23:25 +0530

    set up the app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require("./utils/forecast");
 const geocode = require("./utils/geocode");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Define path for express config
 const publicDirPath = path.join(__dirname, "../public");
@@ -99,8 +100,8 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
 
 //nodemon app.js -e js,hbs   --> everytime the server restarts the template does not load,
